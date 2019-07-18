9b7df358673030c3c0d377127855152803df6f41
Pixlforge <celien@pixlforge.ch>
2019-07-18 17:39:07 +0200

    Refactoring basic abstract filter class

--- app/Filters/Course/AccessFilter.php
+++ app/Filters/Course/AccessFilter.php
@@ -7,9 +7,21 @@ use Illuminate\Database\Eloquent\Builder;
 
 class AccessFilter extends FilterAbstract
 {
+    public function mappings()
+    {
+        return [
+            'free' => true,
+            'premium' => false,
+        ];
+    }
+    
     public function filter(Builder $builder, $value)
     {
-        $value = ($value === 'free' ? true : false);
+        $value = $this->resolveFilterValue($value);
+
+        if (is_null($value)) {
+            return $builder;
+        }
 
         return $builder->where('free', $value);
     }
--- app/Filters/FilterAbstract.php
+++ app/Filters/FilterAbstract.php
@@ -3,8 +3,19 @@
 namespace App\Filters;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 
 abstract class FilterAbstract
 {
     abstract public function filter(Builder $builder, $value);
+
+    public function mappings()
+    {
+        return [];
+    }
+
+    protected function resolveFilterValue($key)
+    {
+        return Arr::get($this->mappings(), $key);
+    }
 }
